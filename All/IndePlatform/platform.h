d580bce672e82bb4ac015d3e04f7d591951eb890
leohawke <lia77593@163.com>
2014-10-04 14:31:47 +0800

    宏命名改动,更多平台检测

--- All/IndePlatform/platform.h
+++ All/IndePlatform/platform.h
@@ -1,7 +1,29 @@
 #ifndef IndePlatform_platform_h
 #define IndePlatform_platform_h
 
-#ifdef WIN32
+#ifdef XENON
+#undef ARCH_XENON
+#define ARCH_XENON
+#endif
+
+#ifdef _M_IX86
+#define ARCH_X86
+#elif defined(_M_IX64)
+#define ARCH_AMD64
+#elif defined(_M_ARM)
+#define ARCH_ARM
+#else
+#error "unsupprot arch"
+#endif
+
+
+#ifndef ARCH_ARM
+#define LM_SSE_INTRINSICS
+#else
+#define LM_ARM_NEON_INTRINSICS
+#endif
+
+#ifdef PLATFORM_WIN32
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
@@ -10,13 +32,14 @@
 #define NOMINMAX
 #endif
 
-#ifndef XENON
+#ifndef ARCH_XENON
+#undef STRICT
 #define STRICT
 #include <Windows.h>
 #include <windowsx.h>
 #endif
 
-#ifdef _MT
+#ifdef MULTI_THREAD
 #include <process.h>
 #endif
 #endif
