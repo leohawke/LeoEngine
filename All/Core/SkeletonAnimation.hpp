3c38d8d92895597612f3291cdc266c7fac688caf
leohawke <lia77593@163.com>
2014-10-28 09:35:14 +0800

    增加新文件

--- All/Core/SkeletonAnimation.hpp
+++ All/Core/SkeletonAnimation.hpp
@@ -4,26 +4,16 @@
 //  Copyright (C), FNS Studios, 2014-2014.
 // -------------------------------------------------------------------------
 //  File name:   Core/Joint.hpp
-//  Version:     v1.01
-//  Created:     8/15/2014 by leo hawke.
+//  Version:     v1.00
+//  Created:     10/28/2014 by leo hawke.
 //  Compilers:   Visual Studio.NET 2013
-//  Description: 提供骨骼层相关逻辑
+//  Description: 提供骨骼层动画相关逻辑
 // -------------------------------------------------------------------------
 //  History:
 //				
 //
 ////////////////////////////////////////////////////////////////////////////
 
-#ifndef Core_Joint_Hpp
-#define Core_Joint_Hpp
-
-#include "..\IndePlatform\LeoMath.h"
-
-namespace leo{
-	struct Joint{
-		float4x4 mInvBindPose;
-		std::size_t mName;
-	};
-}
-
+#ifndef Core_SkeletonAnimation_Hpp
+#define Core_SkeletonAnimation_Hpp
 #endif
