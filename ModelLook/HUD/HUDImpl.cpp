edf3c08ed1d5e2811838ae6e40403a1714136ad1
姚旭 <lia77593@163.com>
2015-12-11 17:01:36 +0800

    今晚任务

--- ModelLook/HUD/HUDImpl.cpp
+++ ModelLook/HUD/HUDImpl.cpp
@@ -1,5 +1,9 @@
 #include "HUDImpl.h"
 
+//实现
+//pc.Target 不会使用
+//实际上，只更新vb，ib
+//不发生drawcall提交
 void leo::HUD::details::DrawText(const std::string & Text, const PaintContext & pc, const Point & offset)
 {
 }
