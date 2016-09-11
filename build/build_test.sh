a4444954fc090d913bd6612ac8377c2be04e285d
leohawke <lia77593@163.com>
2016-09-11 12:44:10 +0800

    fix Engine gcc

---
+++ build/build_test.sh
@@ -0,0 +1,1 @@
+g++ -DINITGUID -DUNICODE -D_UNICODE -I../ -include ../UniversalDXSDK/mingw/sal2.h ../Engine/*.cpp ../Engine/Render/D3D12/*.cpp ../LTest/wWinMain.cpp -L../LBase/build/ -lLBase -oLTest
