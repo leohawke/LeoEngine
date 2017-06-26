43534ff520f111e0a4bb8033329f3e4d22976562
姚旭 <lia77593@163.com>
2017-06-26 17:39:24 +0800

    gcc push

--- build/build_test.sh
+++ build/build_test.sh
@@ -1,5 +1,5 @@
 g++ -DINITGUID -DUNICODE -D_UNICODE -municode -O2 -s -std=c++17\
- -I../ -include ../UniversalDXSDK/mingw/sal2.h \
+ -I../ -I../SDKS -I../SDKS/mingw -include ../SDKS/UniversalDXSDK/mingw/sal2.h \
  ../Engine/*.cpp ../Engine/Render/*.cpp ../Engine/Render/D3D12/*.cpp ../LTest/EngineTest/wWinMain.cpp \
   -L../LBase/build/ -lLBase -oEngineTest \
   -Wl,-subsystem,windows
