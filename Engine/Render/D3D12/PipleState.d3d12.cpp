f4056c92bf3429103b3d1f6bedc0b27918ab8b65
姚旭 <lia77593@163.com>
2018-03-26 13:22:59 +0800

    fix SimpleMask state lost

--- Engine/Render/D3D12/PipleState.d3d12.cpp
+++ Engine/Render/D3D12/PipleState.d3d12.cpp
@@ -12,6 +12,11 @@ namespace platform_ex::Windows::D3D12 {
 		graphics_ps_desc.BlendState = Convert(base::BlendState);
 		graphics_ps_desc.RasterizerState = Convert(base::RasterizerState);
 		graphics_ps_desc.DepthStencilState = Convert(base::DepthStencilState);
+		graphics_ps_desc.SampleMask = base::BlendState.sample_mask;
+		graphics_ps_desc.NodeMask = 0;
+		graphics_ps_desc.CachedPSO.pCachedBlob = nullptr;
+		graphics_ps_desc.CachedPSO.CachedBlobSizeInBytes = 0;
+		graphics_ps_desc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
 	}
 	leo::observer_ptr<ID3D12PipelineState> PipleState::RetrieveGraphicsPSO(const platform::Render::InputLayout & _layout, ShaderCompose & shader_compose, const std::shared_ptr<platform::Render::FrameBuffer>& _frame, bool HasTessellation) const
 	{
